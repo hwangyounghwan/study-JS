49a218b7ddd2861659acacd017876f690badad02
Hwang Young Hwan <food019@gmail.com>
2022-04-13 00:12:07 +0900

    IMP: Operation.js 내용 추가

--- operator.js
+++ operator.js
@@ -23,3 +23,40 @@
 
 // 피연산자가 값의 역할을 한다면 연산자는 피연산자를 연산하여 새로운 값을 만든다 라는 동사의 역할을 한다고 볼 수 있다.
 
+// 대소 비교 연산자
+//1. === 은 타입과 값 전부 비교한다.
+//2. == 은 값만 비교하게된다
+
+//ex 5 == 5; -> true;
+
+// 5 == '5' -> true;
+console.log ('5 == 5')
+console.log (5 == 5); // -> true;
+
+console.log("5 == '5'")
+console.log(5 == '5'); // -> true;
+
+// 동등 비교, 결과를 예측하기 어렵다.
+
+console.log("'0' == ''");
+console.log ('0' == ''); // -> false;
+
+console.log("0 == ''");
+console.log(0 == ''); // -> true;
+
+console.log("false == 'false'")
+console.log(false == 'false');
+console.log("false == null");
+console.log(false == null);
+
+//Object.is 메서드
+// ES6에서 도입된 Object.is 메서드는 다음과 같이 예측 가능한 정확한 비교값을 반환한다. 그외에는 일치 비교 연산자와 동일하게 동작한다.
+//ex
+console.log (-0 === +0); // -> true
+console.log(Object.is(-0, +0)); // -> false
+
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(Object.is(NaN,NaN));
+
+
