d20f177a6f184a52c1c061d9046e874f9e33e536
황영환 <younghwan.hwang@ikoob.com>
2022-09-09 13:54:27 +0900

    NEW: switch 추가

--- control_flow_statement.js
+++ control_flow_statement.js
@@ -24,3 +24,55 @@ if(x <0) {
 function sum(a,b) {
   return a+b;
 }
+
+// switch 문
+// switch 문은 주어진 표현식을 평가하여 그 값과 일치하는 표현식을 갖는 case 문으로 실행 흐름을 옮긴다.
+// case 문은 상황을 의미하는 표현식을 지정하고 콜론으로 마친다. 그리고 드 뒤에 실행할 문들을 위치시킨다.
+// switch 문의 표현식과 일치하는 case 문이 없다면 실행 순서는 default 문으로 이동한다. default 문은 선택 사항으로, 사용할수 있고 사용하지 않을 수도 있다.
+
+/**
+ * switch (표현식) {
+ *     case 표현식1:
+ *     switch 문의 표현식과 표현식1이 일치하면 실행시킬 문;
+ *     break;
+ *     case 표현식2:
+ *     switch 문의 표현식과 표현식1이 일치하면 실행시킬 문;
+ *     break;
+ *     default:
+ *     switch 문의 표현식과 일치하는 case 문이 없을 때 실행시킬 문;
+ * }
+ */
+
+// 예제 about month
+let month = 11;
+let monthName;
+
+switch(month) {
+  case 1: monthName = 'January';
+    break;
+  case 2: monthName = 'Februry';
+    break;
+  case 3: monthName = 'March';
+    break;
+  case 4: monthName = 'April';
+    break;
+  case 5: monthName = 'May';
+    break;
+  case 6: monthName = 'June';
+    break;
+  case 7: monthName = 'July';
+    break;
+  case 8: monthName = 'August';
+    break;
+  case 9: monthName = 'September';
+    break;
+  case 10: monthName = 'October';
+    break;
+  case 11: monthName = 'November';
+    break;
+  case 12: monthName = 'December';
+    break;
+  default: monthName = 'invalidMonth'
+}
+
+console.log(monthName);
